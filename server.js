e123f404670502ad44a830109660df902a6d2f42
sebciulina <76162501+sebciulina@users.noreply.github.com>
2025-12-06 18:18:54 +0100

    Fix wildcard route for production

--- server.js
+++ server.js
@@ -10,12 +10,8 @@ connectDB();
 
 const app = express();
 
-app.use(cors());                 
-app.use(express.json());         
-
-app.get('/', (req, res) => {
-  res.send('API Wypożyczalni działa poprawnie!');
-});
+app.use(cors());
+app.use(express.json());
 
 app.use('/api/cars', require('./routes/cars'));
 app.use('/api/reservations', require('./routes/reservations'));
@@ -26,7 +22,7 @@ const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/client/build')));
 
-  app.get('*', (req, res) => {
+  app.get(/(.*)/, (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 } else {
